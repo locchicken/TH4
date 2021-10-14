105acd633a36db9c79151e1f6800070f64c0eba5
Your Name <you@example.com>
2021-10-14 19:32:40 +0700

    Nhat Loc

---
+++ ASC.Utilities/ObjectExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace ASC.Utilities
+{
+    public static class ObjectExtension
+    {
+        public static T CopyObject<T>(this object objSource)
+        {
+            var serialized = JsonConvert.SerializeObject(objSource);
+            return JsonConvert.DeserializeObject<T>(serialized);
+        }       
+    }
+}
---
+++ Project/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using ASC.Utilities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Project.Models;
+using Project.Web.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+       
+        private IOptions<ApplicationSettings> _settings;
+        public HomeController(IOptions<ApplicationSettings> settings)
+        {
+            _settings = settings;
+        }
+
+        public HomeController()
+        {
+        }
+
+        public IActionResult Index()
+        {
+            // Set Session
+            HttpContext.Session.SetSession("Test", _settings.Value);
+            // Get Session
+            var settings = HttpContext.Session.GetSession<ApplicationSettings>("Test");
+            // Usage of IOptions
+            ViewBag.Title = _settings.Value.ApplicationTitle;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    
+         public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
